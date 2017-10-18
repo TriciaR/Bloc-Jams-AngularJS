eb176fdeb4c5be83489899589c2377b45ed01e9d
Patricia Rogers <Patricia.Rogers29@gmail.com>
2017-10-17 20:07:11 -0400

    cp4-fin

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,11 @@
 (function() {
   function config($stateProvider, $locationProvider) {
     $locationProvider
-    .html5Mode({
-        enabled: true,
-        requireBase: false
-    });
-    $stateProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        })
+     $stateProvider
     .state('landing', {
         url: '/',
         templateUrl: '/templates/landing.html'
