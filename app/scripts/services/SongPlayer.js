e37225d3399dadb6f37b354c9052093238da6df0
Patricia Rogers <Patricia.Rogers29@gmail.com>
2017-10-24 22:59:58 -0400

    showing pause button

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,12 +2,27 @@
     function SongPlayer() {
         var SongPlayer = {};
 
-        SongPlayer.play = function (song) {
-            var currentBuzzObject = new buzz.sound(song.audioUrl, {
+            var currentSong = null;
+            var currentBuzzObject = null;
+
+            SongPlayer.play = function (song) {
+                if (currentSong !== song) {
+                    if (currentBuzzObject) {
+                        currentBuzzObject.stop();
+                        currentSong.playing = null;
+                    }
+                } else if (currentSong === song) {
+                    if (currentBuzzObject.isPaused()) {
+                        currentBuzzObject.play();
+                    }
+                }
+
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
             currentBuzzObject.play();
+                song.playing = true;
         }
 
 
