ce6ae4ad57b83154c6d0eeda1de48af958cde46b
Patricia Rogers <Patricia.Rogers29@gmail.com>
2017-10-29 22:31:29 -0400

    cleanAs8

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,23 +1,24 @@
 (function () {
     /**
-     * @function SongPlayer
-     * @description allows controlling the songs & audio files from Fixtures through Player Bar
-     * @param {Object} Fixtures
-     */
+    * @function SongPlayer
+    * @description allows controlling the songs & audio files from Fixtures through Player Bar
+    * @param {Object} Fixtures
+    */
     function SongPlayer(Fixtures) {
         var SongPlayer = {};
         /**
-         * @desc sets active album
+        * @desc sets active album
         */
         var currentAlbum = Fixtures.getAlbum();
-             /**
-             * @desc Buzz object audio file
-             * @type {Object}
-             */
+
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
         var currentBuzzObject = null;
 
 
-            /**            * @function setSong            * @desc Stops currently playing song and loads new audio file as currentBuzzObject            * @param {Object} song            */
+        /**        * @function setSong        * @desc Stops currently playing song and loads new audio file as currentBuzzObject        * @param {Object} song        */
         var setSong = function (song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
@@ -32,39 +33,47 @@
         };
 
         /**
-         * @func getSongIndex
-         * @desc finds the index of the selected song
-         * @param {object} song
-         * @return {number}
-         */
+        * @func getSongIndex
+        * @desc finds the index of the selected song
+        * @param {object} song
+        * @return {number}
+        */
         var getSongIndex = function (song) {
             return currentAlbum.songs.indexOf(song);
         };
 
-            /**
-            * @desc creates public attr SongPlayer.currentSong  set active song from list of songs
-            * @type {Object}
-            */
+        /**
+        * @desc creates public attr SongPlayer.currentSong  set active song from list of songs
+        * @type {Object}
+
+        */
+        /**
+        * @desc clears current song
+        */
         SongPlayer.currentSong = null;
+
 
         var playSong = function () {
             currentBuzzObject.play();
             song.playing = true;
         };
-
+        /**
+        * @desc creates public attr SongPlayer.currentSong  set active song from list of songs
+        * @type {Object}
+        */
         var stopSong = function () {
             currentBuzzObject.stop();
             SongPlayer.currentSong.playing = null;
         };
 
 
-            /**            * @function SongPlayer.play            * @desc plays currently selected song and loads selected audio file as currentBuzzObject if not currentSong            * @param {Object} song            */
+        /**        * @function SongPlayer.play        * @desc plays currently selected song and loads selected audio file as currentBuzzObject if not currentSong        * @param {Object} song        */
         SongPlayer.play = function (song) {
 
-                /**
-                * @function playSong
-                * @desc plays currenty selected song audio file
-                */
+            /**
+            * @function playSong
+            * @desc plays currenty selected song audio file
+            */
             var playSong = function () {
                 currentBuzzObject.play();
                 song.playing = true;
@@ -82,7 +91,7 @@
             }
         };
 
-            /**            * @function SongPlayer.pause            * @desc Pauses currently playing            * @param {Object} song            */
+        /**        * @function SongPlayer.pause        * @desc Pauses currently playing        * @param {Object} song        */
 
         SongPlayer.pause = function (song) {
             song = song || SongPlayer.currentSong;
@@ -106,6 +115,10 @@
             }
         };
 
+        /**
+        * @func SongPlayer.next
+        * @desc stop current song, advance & play next indexed song in current album
+        */
         SongPlayer.next = function () {
             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
             currentSongIndex++;
